ba63ffd483267bab1dd4294bb9375898deeaeee4
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-01-07 09:41:57 +0000

    Day and night Cycle and Big wall done

--- Sem Nome/js/world.js
+++ Sem Nome/js/world.js
@@ -2,10 +2,12 @@
 let scene, renderer, camera;
 
 //Variáveis básicas para a cidade
-let terrainPlane, rua, passeio, passeio2;
+let terrainPlane, pavimento1, pavimento2, passeio, passeio2;
 
 //Variáveis utilizadas para a mudança de metrologia na cidade
-let sol, luzAmbiente;
+let sol, luzAmbiente, solCamera;
+
+let solPotencia = 1;
 
 //Variáveis usadas para o sistema de ruas da cidade
 let passeioMain;
@@ -23,7 +25,7 @@ window.onload = function init() {
 
     //Camera
     camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(-270, 60, 40)
+    camera.position.set(-570, 400, 10)
     scene.add(camera)
 
     //Renderer
@@ -40,9 +42,10 @@ window.onload = function init() {
     //Creating the world
     renderStuff()
     createTerrain()
-    createInfrastructure()
+    createParkingLot()
     createLight();
     createTemporaryCar()
+    sunUpdate()
 
 
 }
@@ -58,13 +61,15 @@ function createTemporaryCar() {
         })
 
     );
-    car.position.set(0, 1, 16)
+    car.receiveShadow = true;
+    car.castShadow = true;
+    car.position.set(0, 9, 16)
     scene.add(car);
 }
 //Função para criar o terreno da simulação
 function createTerrain() {
     terrainPlane = new Physijs.PlaneMesh(
-        new THREE.PlaneGeometry(500, 500, 2),
+        new THREE.PlaneGeometry(2000,2000, 2),
         new THREE.MeshPhongMaterial({
             //FIXME: TEMPORARY COLORS
             color: 0x79A43D,
@@ -72,7 +77,7 @@ function createTerrain() {
         })
     );
 
-    terrainPlane.position.set(0, -8, 0);
+    terrainPlane.position.set(0, 0, 0);
     terrainPlane.rotateX(Math.PI / 2);
     scene.add(terrainPlane);
     //Podes alterar isto para olhar para o Carro (YARICK)
@@ -81,14 +86,15 @@ function createTerrain() {
 
 }
 
-//Função para criar o terreno da simulação
-function createInfrastructure() {
+//Função para criar os detalhes  da simulação
+function createParkingLot() {
     //ATENÇÃO todos os bumpmaps e texturas não são as oficiais ainda(APENAS PARA TESTE)
-    //Road
+    // Area do parque de estacionamento
+    let muro1, muro2, muro3;
+    let chao = new THREE.Object3D()
     let bumpmapTexture = new THREE.TextureLoader().load("./Textures/Road_bump_map_temp.jpg");
-
-    rua = new Physijs.BoxMesh(
-        new THREE.BoxGeometry(300, 2, 66),
+    pavimento1 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(250, 2, 350),
         new THREE.MeshPhongMaterial({
             //FIXME: TEMPORARY COLORS
             color: 0x444444,
@@ -97,52 +103,108 @@ function createInfrastructure() {
         })
 
     );
-    scene.add(rua);
-    rua.position.set(0, -8, 0)
-
-    //Sidewalk
-    passeioMain = new THREE.Object3D()
-
-    bumpmapTexture = new THREE.TextureLoader().load("./textures/Sw_bump_map_temp.jpg")
-    passeio = new Physijs.BoxMesh(
-        new THREE.BoxGeometry(300, 2, 10),
+    pavimento1.position.set(0, 0, 0)
+    chao.add(pavimento1);
+    //Area da zona de entregas
+    pavimento2 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(200, 2, 150),
         new THREE.MeshPhongMaterial({
             //FIXME: TEMPORARY COLORS
-            color: 0x999999,
+            color: 0x444444,
             bumpMap: bumpmapTexture,
-            bumpScale: 0.2,
+            bumpScale: 0.20
+        })
+
+    );
+    pavimento2.position.set(220, 0, -100);
+    pavimento1.receiveShadow = true;
+    pavimento2.receiveShadow = true;
+    chao.add(pavimento2);
+    scene.add(chao);
+
+    
+    //Walls
+    bumpmapTexture = new THREE.TextureLoader().load("./Textures/brickwall_bump_map_temp.jpg");
+    let muroObjeto = new THREE.Object3D()
+    muro1 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(10, 30, 150),
+        new THREE.MeshPhongMaterial({
+            //FIXME: TEMPORARY COLORS
+            color: 0xE06666,
+            bumpMap: bumpmapTexture,
+            bumpScale: 0.20
+        })
+
+    );
+    muro1.position.set(320, 16, -100)
+
+    muro2 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(450, 30, 10),
+        new THREE.MeshPhongMaterial({
+            //FIXME: TEMPORARY COLORS
+            color: 0xE06666,
+            bumpMap: bumpmapTexture,
+            bumpScale: 0.20
+        })
+
+    );
+    muro2.position.set(100, 16, -170)
+
+    muro3 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(10, 30, 290),
+        new THREE.MeshPhongMaterial({
+            //FIXME: TEMPORARY COLORS
+            color: 0xE06666,
+            bumpMap: bumpmapTexture,
+            bumpScale: 0.20
+        })
+
+    );
+    muro3.position.set(-120, 16, 30)
+
+    muro4 = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(250, 30, 10),
+        new THREE.MeshPhongMaterial({
+            //FIXME: TEMPORARY COLORS
+            color: 0xE06666,
+            bumpMap: bumpmapTexture,
+            bumpScale: 0.20
         })
 
     );
 
-    passeio2 = new Physijs.BoxMesh(
-        new THREE.BoxGeometry(300, 2, 10),
-        new THREE.MeshPhongMaterial({
-            //FIXME: TEMPORARY COLORS
-            color: 0x999999,
-            bumpMap: bumpmapTexture,
-            bumpScale: 0.2,
-        })
+    muro4.position.set(0, 16, 170)
+    muro1.castShadow = true;
+    muro2.castShadow = true;
+    muro3.castShadow = true;
+    muro4.castShadow = true;
+    muroObjeto.add(muro1);
+    muroObjeto.add(muro2);
+    muroObjeto.add(muro3);
+    muroObjeto.add(muro4);
+    scene.add(muroObjeto);
 
-    );
-
-    passeio.position.set(-5, -7, 36);
-    passeio2.position.set(-5, -7, -36);
-    passeioMain.add(passeio);
-    passeioMain.add(passeio2);
-    scene.add(passeioMain);
-    console.log("Infraestrutura criada com sucesso!")
 }
 
 //Função para criar as luzes da simulação
 function createLight() {
 
-    sol = new THREE.DirectionalLight(0xC3C3C3, 0.5);
+    solCamera = new THREE.OrthographicCamera(-300, 300, 300, -300, 1, 1200);
+    scene.add(solCamera);
+
+    sol = new THREE.DirectionalLight(0xC3C3C3, solPotencia);
     scene.add(sol);
-    //FIXME:Changes to the position will be needed 
-    sol.position.set(0, 2000, 0)
-    //Adicionar o target
     scene.add(sol.target)
+    sol.position.set(0, 2000, 0)
+    //Sombras
+    sol.castShadow = true;
+    sol.autoUpdate = true;
+    sol.shadow.camera = solCamera;
+
+    //Settings importantes para as sombras
+    sol.shadow.bias = 0.00020
+    sol.shadow.camera.near = 10;
+    sol.shadow.camera.far = 2100;
 
     //FIXME: REMOVE LATER
     const helper = new THREE.DirectionalLightHelper(sol, 5);
@@ -150,11 +212,45 @@ function createLight() {
 
     //ATENÇÃO TEMP LIGHT
     //FIXME: Change the lighting
-    luzAmbiente = new THREE.AmbientLight(0xFFFFFF, 1); // soft white light
+    luzAmbiente = new THREE.AmbientLight(0xEEEEEE, 0.2); // soft white light
     scene.add(luzAmbiente);
     luzAmbiente.position.set(0, 50, 0)
 
     console.log("Luzes criadas com sucesso")
+}
+
+function sunUpdate() {
+    //Cada dia e noite demoram 6.6 minutos
+    //80 vezes cada if 
+    //TODO: O sunPower devia passar a 1 outra vez
+    setInterval(() => {
+        sol.autoUpdate = true;
+        sol.needsUpdate = true;
+        if ((sol.position.z < 2000 && sol.position.z >= 0) && (sol.position.y <= 2000 && sol.position.y > 0)) {
+            //Day
+            sol.position.set(sol.position.x, sol.position.y - 25, sol.position.z + 25);
+            solPotencia = solPotencia - 0.0125;
+            sol.power = solPotencia * 4 * Math.PI;
+        } else if (sol.position.z > 0) {
+            //Night
+            sol.position.set(sol.position.x, sol.position.y - 25, sol.position.z - 25);
+            solPotencia = 0;
+            sol.power = solPotencia * 4 * Math.PI;
+            sol.castShadow = false;
+        } else if (sol.position.z <= 0 && sol.position.y < 0) {
+            // Night
+            sol.position.set(sol.position.x, sol.position.y + 25, sol.position.z - 25);
+            sol.power = solPotencia * 4 * Math.PI;
+        } else if ((sol.position.z >= -2000 && sol.position.z < 0) && sol.position.y >= 0) {
+            //Day
+            sol.position.set(sol.position.x, sol.position.y + 25, sol.position.z + 25);
+            solPotencia = solPotencia + 0.0125;
+            sol.power = solPotencia * 4 * Math.PI;
+            sol.castShadow = true
+        }
+        console.log(sol.position)
+
+    }, 5000);
 }
 
 //Função do Render
